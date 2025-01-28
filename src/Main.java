5049c1612020a887dde84654e1294bcae7c7d307
AdnerC <adnerc@Nycstudents.net>
2025-01-28 11:01:57 -0500

    Worked on Part 1 of the Unit 6 project which involves splitting the "hand" of the user.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,43 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String[] fileData = getFileData("src/input_file");
+        String[] numbers = new String[0];
+        String line = "";
+        for (int i = 0; i < fileData.length; i++) {
+            line = fileData[i];
+            int bid = Integer.parseInt(getBidAmount(line));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        }
+        for(int i = 0; i < line.length(); i++){
+            int bar = line.indexOf("|");
+
+            numbers = line.split(",");
+            System.out.println(Arrays.toString(numbers));
         }
     }
+    public static String[] getFileData(String fileName) {
+        String fileData = "";
+        try {
+            File f = new File(fileName);
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()) {
+                fileData += s.nextLine() + "\n";
+            }
+            return fileData.split("\n");
+        }
+        catch (FileNotFoundException e) {
+            return null;
+        }
+    }
+
+    public static String getBidAmount(String line){
+        int bar = line.indexOf("|");
+        return line.substring(bar + 1);
+    }
+
 }
